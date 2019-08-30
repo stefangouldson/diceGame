b1a2a765110f1ae209e4ce5f0ba3748272181041
CN0103 <cn0103@CN103.local>
2019-08-30 13:43:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//random number between 1-6
+const diceNum=()=>{
+    let x=(6*Math.random())+1
+    let y=Math.floor(x)
+    console.log(y)
+}
+
+//random img
+    //define images in js
+let Img1=document.getElementById("dice1")
+let Img2=document.getElementById("dice2")
+let Img3=document.getElementById("dice3")
+let Img4=document.getElementById("dice4")
+let Img5=document.getElementById("dice5")
+let Img6=document.getElementById("dice6")

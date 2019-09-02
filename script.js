75359401f9c90b8c9efa5f0f3bde0218cb85e240
CN0103 <cn0103@CN103.local>
2019-09-02 17:06:45 +0100

    finished version

--- script.js
+++ script.js
@@ -1,15 +1,169 @@
-//random number between 1-6
-const diceNum=()=>{
-    let x=(6*Math.random())+1
-    let y=Math.floor(x)
-    console.log(y)
-}
 
 //random img
     //define images in js
-let Img1=document.getElementById("dice1")
-let Img2=document.getElementById("dice2")
-let Img3=document.getElementById("dice3")
-let Img4=document.getElementById("dice4")
-let Img5=document.getElementById("dice5")
-let Img6=document.getElementById("dice6")
+    let imageInput=document.getElementById("canvas")
+
+
+let randArr=[
+    "./img/dice1.png",
+    "./img/dice2.png",
+    "./img/dice3.png",
+    "./img/dice4.png",
+    "./img/dice5.png",
+    "./img/dice6.png"]
+
+let total=0
+let input1=document.getElementById("input1")
+let input2=document.getElementById("score")
+
+function getRandomImg(imgArr){
+    let x=(6*Math.random())
+    let y=Math.floor(x)
+    img=imgArr[y]
+
+    if (y==0){
+        input2.textContent="You lose! Try again?"
+        total=0
+        imageInput.src=img
+    }
+    else if(y==1){total+=2
+        input2.textContent="Keep going"
+        imageInput.src=img}
+    else if(y==2){total+=3
+        input2.textContent="Keep going"
+        imageInput.src=img}
+    else if(y==3){total+=4
+        input2.textContent="Keep going"
+        imageInput.src=img}
+    else if(y==4){total+=5
+        input2.textContent="Keep going"
+        imageInput.src=img}
+    else if(y==5){total+=6
+        input2.textContent="Keep going"
+        imageInput.src=img}
+    else{
+        alert("error")
+        console.error("Somethings wrong")
+    } console.log(total)
+    
+    if (total>20){
+        alert("You win!")
+        input2.textContent="You won! Roll again?"
+        total=0
+    } input1.textContent=total;
+}
+
+//splash page
+
+document.getElementById("singleplayer").style.display="none"
+document.getElementById("multiplayer").style.display="none"
+
+open1.addEventListener("click",()=>{
+    document.getElementById("splash-container").style.display="none"
+    document.getElementById("singleplayer").style.display="block"
+})
+
+open2.addEventListener("click",()=>{
+    document.getElementById("splash-container").style.display="none"
+    document.getElementById("multiplayer").style.display="block"
+})
+
+
+//multiplayer
+
+//player 1
+
+let imageInput1=document.getElementById("canvas1")
+
+let total1=0
+let inputP1=document.getElementById("scoreTotal1")
+let inputP2=document.getElementById("game")
+
+function getRandomImg1(imgArr){
+    let x=(6*Math.random())
+    let y=Math.floor(x)
+    img=imgArr[y]
+
+    if (y==0){
+        inputP2.textContent="You lose! Try again?"
+        total1=0
+        total2=0
+        imageInput1.src=img
+    }
+    else if(y==1){total1+=2
+        inputP2.textContent="Keep going"
+        imageInput1.src=img
+    }
+    else if(y==2){total1+=3
+        inputP2.textContent="Keep going"
+        imageInput1.src=img}
+    else if(y==3){total1+=4
+        inputP2.textContent="Keep going"
+        imageInput1.src=img}
+    else if(y==4){total1+=5
+        inputP2.textContent="Keep going"
+        imageInput1.src=img}
+    else if(y==5){total1+=6
+        inputP2.textContent="Keep going"
+        imageInput1.src=img}
+    else{
+        alert("error")
+        console.error("Somethings wrong")
+    } console.log(total)
+    
+    if (total1>20){
+        alert("Player 1 wins!")
+        inputP2.textContent="You won! Roll again?"
+        total1=0
+        total2=0
+    } inputP1.textContent=total1;
+}
+
+
+//player 2
+
+let imageInput2=document.getElementById("canvas2")
+
+let total2=0
+let inputA1=document.getElementById("scoreTotal2")
+let inputA2=document.getElementById("game1")
+
+function getRandomImg2(imgArr){
+    let x=(6*Math.random())
+    let y=Math.floor(x)
+    img=imgArr[y]
+
+    if (y==0){
+        inputA2.textContent="You lose! Try again?"
+        total1=0
+        total2=0
+        imageInput2.src=img
+    }
+    else if(y==1){total2+=2
+        inputA2.textContent="Keep going"
+        imageInput2.src=img
+    }
+    else if(y==2){total2+=3
+        inputA2.textContent="Keep going"
+        imageInput2.src=img}
+    else if(y==3){total2+=4
+        inputA2.textContent="Keep going"
+        imageInput2.src=img}
+    else if(y==4){total2+=5
+        inputA2.textContent="Keep going"
+        imageInput2.src=img}
+    else if(y==5){total2+=6
+        inputA2.textContent="Keep going"
+        imageInput2.src=img}
+    else{
+        alert("error")
+        console.error("Somethings wrong")
+    } console.log(total2)
+    
+    if (total2>20){
+        alert("Player 2 wins!")
+        inputA2.textContent="You won! Roll again?"
+        total1=0
+        total2=0
+    } inputA1.textContent=total2;
+}
